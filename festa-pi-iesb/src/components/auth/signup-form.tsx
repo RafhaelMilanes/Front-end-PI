34d9ac7f23d0aeaddd7bca4d3e138f183f8f7eaf
Rafhael Milanes <rafhael.milanes@lekto.com.br>
2024-10-05 23:34:51 -0300

    adiciona componete Signup #6. Closes #6

---
+++ festa-pi-iesb/src/components/auth/signup-form.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+import { useState } from "react";
+import { Input } from "../ui/input";
+import { faEnvelopeOpen } from "@fortawesome/free-solid-svg-icons";
+import { Button } from "../ui/button";
+import { faPerson } from "@fortawesome/free-solid-svg-icons/faPerson";
+
+export const SignupForm = () => {
+    const router = useRouter();
+    const [nameField, setNameField] = useState("")
+    const [emailField, setEmailField] = useState("@gmail.com")
+    const [passwordField, setPasswordField] = useState("")
+
+    const handleEnterButton = () => {
+        router.replace("/home")
+    }
+
+    return (
+        <>
+            <Input
+                placeholder="Digite seu nome"
+                icon={faPerson}
+                value={nameField}
+                onChange={t => setNameField(t)}
+            />
+            <Input
+                placeholder="Digite seu e-mail"
+                icon={faEnvelopeOpen}
+                value={emailField}
+                onChange={t => setEmailField(t)}
+            />
+            <Input
+                placeholder="Digite sua senha"
+                value={passwordField}
+                onChange={t => setPasswordField(t)}
+                password
+            />
+
+            <Button label="Criar conta" onClick={handleEnterButton} size={1}/>
+        </>
+    )
+}
