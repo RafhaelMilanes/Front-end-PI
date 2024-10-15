cfc77dce25f66a6fd6049a31bf91f4bf72e26e07
Rafhael Milanes <rafhael.milanes@lekto.com.br>
2024-10-14 23:47:49 -0300

    atualiza page.tsx

--- festa-pi-iesb/src/app/page.tsx
+++ festa-pi-iesb/src/app/page.tsx
@@ -1,7 +1,24 @@
+"use client"
+import { Logo } from "@/components/ui/logo";
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
 export default function Page() {
-  return(
-    <div>
-    ...
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push('/home');
+    }, 3000);
+    
+    return () => clearTimeout(timer);
+  }, [router]);
+
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-[#97A2D7] ">
+      <div className="md:animate-pulse ">
+        <Logo  size={80} />
+      </div>
     </div>
-  )
+  );
 }
