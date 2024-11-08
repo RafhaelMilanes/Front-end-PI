bc0ecffeb43a08dd3382233d3e29985590079ba6
Pedro24M <exacadrillex@gmail.com>
2024-11-08 16:19:18 -0300

    Criação do componente de Proximas festas na home Close #24

--- festa-pi-iesb/src/app/(index)/home/page.tsx
+++ festa-pi-iesb/src/app/(index)/home/page.tsx
@@ -1,11 +1,17 @@
 import AgendaMinimizada from "@/components/ui/agendaMinimizada";
 import NavBar from "@/components/ui/navBar";
+import SecaoProximasFestas from "@/components/ui/secaoProximasFestas";
 
 export default function Page() {
     return(
         <>
             <NavBar />
             <AgendaMinimizada />
+            <div className="flex flex-col pt-10">
+            <h1 className="self-center text-white text-7xl">Início</h1>
+            <SecaoProximasFestas />
+            <section><h2>afcsjklfclhck</h2></section>
+            </div>
         </>
     )
 }
