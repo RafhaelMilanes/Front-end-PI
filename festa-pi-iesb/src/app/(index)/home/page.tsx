d6b3b265fb17b379f88cf22a60480165006b18d0
Pedro24M <exacadrillex@gmail.com>
2024-11-12 15:58:43 -0300

    Comclusão da pagina inicial Close #29 #30 #31

--- festa-pi-iesb/src/app/(index)/home/page.tsx
+++ festa-pi-iesb/src/app/(index)/home/page.tsx
@@ -1,5 +1,7 @@
 import AgendaMinimizada from "@/components/ui/agendaMinimizada";
+import CriarNovaFesta from "@/components/ui/criarNovaFesta";
 import NavBar from "@/components/ui/navBar";
+import SecaoFestasJáMarcadas from "@/components/ui/secaoFestasJáMarcadas";
 import SecaoProximasFestas from "@/components/ui/secaoProximasFestas";
 
 export default function Page() {
@@ -10,9 +12,8 @@ export default function Page() {
       <div className="flex flex-col pt-10">
         <h1 className="self-center text-white text-7xl">Início</h1>
         <SecaoProximasFestas />
-        <section>
-          <h2>afcsjklfclhck</h2>
-        </section>
+        <SecaoFestasJáMarcadas />
+        <CriarNovaFesta />
       </div>
     </>
   );
