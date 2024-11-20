06218268dba6c3bca09dc85d7ce48a262690a430
Pedro24M <exacadrillex@gmail.com>
2024-11-20 17:56:57 -0300

    Criação da base para o Banco de dados Close#49

--- festa-pi-iesb/src/app/layout.tsx
+++ festa-pi-iesb/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-import { TelaProvaider } from "../context/TelaContext";
 
 export const metadata: Metadata = {
   title: "Social Party",
